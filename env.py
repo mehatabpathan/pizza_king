cab728bc3d8b2364451ed82bcfa41b26e7b76121
Mehatab Pathan <mehatab@Mehatabs-Air.station>
2024-01-18 12:33:38 +0000

    adding new files

---
+++ env.py
@@ -0,0 +1,9 @@
+import os
+os.environ["STRIPE_PUBLIC_KEY"]="pk_live_51OJxlVH8Vqa8Gi3UBse2cUwlKlRld9Y4WFZVZ0To43vp2Lo2TI2NmPMZi0v7kipwydoWRlAzfz2Nhqx7SKnFSqpv00xEiuuESE"
+os.environ["STRIPE_SECRET_KEY"]="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"
+os.environ["STRIPE_WH_SECRET"]="whsec_yPlbCIQx1YO3nNmat9mVawHR1iBeqZDS"
+os.environ["DATABASE_URL"]="postgres://tevpnujm:dX_Uq2WLj8KHLtbVQa7cp1nGjGvku6t5@manny.db.elephantsql.com/tevpnujm"
+os.environ["EMAIL_HOST_USER"]="mehatab.pathan231@gmail.com"
+os.environ["EMAIL_HOST_PASS"]="symytioklgcrdiiw"
+os.environ["CLOUDINARY_URL"]="cloudinary://816261457542735:e2H-2zdWjTrFphGsOtAR58eAoDM@mehatab"
+
